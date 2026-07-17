b5e2dcd45277449215c671db3843dd68b9b56861
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-17 12:10:07 +0530

    Update First occurance in shorted.cpp

--- arrays /First occurance in shorted.cpp
+++ arrays /First occurance in shorted.cpp
@@ -1,1 +1,30 @@
-
+class Solution {
+  public:
+    int firstSearch(vector<int> &arr, int k) {
+        // code here
+        int start = 0;
+        int end  = arr.size()-1;
+        int ans = -1;
+        while (start <=end ){
+            int mid= start+(end-start)/2;
+          
+           if  (k== arr[mid]){
+                ans = mid ;
+                end = mid - 1;
+           }
+           else  if (k>arr[mid]){
+                start =mid + 1;
+            }
+            else {
+                end = mid -1 ;
+            }
+            
+            
+        
+ ;
+    
+            
+        }      
+        return ans; 
+    }
+};
