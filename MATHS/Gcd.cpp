5fc4c497dc9638d0cf1c7a22ac6c36ca84eb6633
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-18 10:24:46 +0530

    Update Gcd.cpp

--- MATHS/Gcd.cpp
+++ MATHS/Gcd.cpp
@@ -2,17 +2,17 @@ class Solution {
   public:
     int gcd(int a, int b) {
         // code here
-      while (a>0 && b>0 ){
-          if (a>b){
-          a = a%b ;
+      while (a>0 && b>0 ){// check if a and b greater then 0
+          if (a>b){ // agar a bada hai b se 
+          a = a%b ; // then a hojayega a%b 
           }
           else {
-              b = b%a ;
+              b = b%a ; // agar a chote hai 
           }
           
           }
-          if (a== 0) return b;
-          return a;
+          if (a== 0) return b; // agaar a zero hai to ans b ho jayega 
+          return a; // agar b = 0 hai to and a ho jayega 
       }
     
 };
