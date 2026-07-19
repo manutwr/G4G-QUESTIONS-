6c8994e63ed862fb881e10cdb397d261115c209d
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-19 11:06:32 +0530

    Update Gcd and lcm.cpp

--- MATHS/Gcd and lcm.cpp
+++ MATHS/Gcd and lcm.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     // Helper function to calculate GCD using Euclidean algorithm
     int findGcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
+        while (a>0 && b>0 ){
+            if (a>b){
+                a= a%b;
+            }
+            else {
+                b= b%a;
+            }
+            
         }
-        return a;
+          if (a==0)return b; // iss statement ko hamesha loop ke bhr rkhna hai 
+            return a;
+        
+        // for finding gcd 
     }
 
     vector<int> lcmAndGcd(int a, int b) {
-        int gcd = findGcd(a, b);  // calling the gcd function 
+        int gcd = findGcd(a, b);
         
         // Calculate LCM using long long to avoid any potential overflow
         int  lcm = (a * b) / gcd;
