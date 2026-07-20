c69ca7b181958c9b4246422aeeb0d49898ee8786
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-20 13:02:08 +0530

    Update Cheak if array is shorted or not.cpp

--- arrays /Cheak if array is shorted or not.cpp
+++ arrays /Cheak if array is shorted or not.cpp
@@ -2,12 +2,12 @@ class Solution {
   public:
     bool isSorted(vector<int>& arr) {
         // code here
-        for (int i=0 ; i<arr.size()-1 ; i++ ){
-            if (arr[i]> arr [i+1]){
+        for (int i=0 ; i<arr.size()-1 ; i++ ){ // loop chalaon 
+            if (arr[i]> arr [i+1]){ // agar array i bada hai i+1 se to false return kar do
                 return false ;
             }
             
         }
-        return true;
+        return true; // nahi to true
     }
 };
