d40280e5e71ecaa30d021d75ccfa152200be7bfc
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-21 14:17:43 +0530

    Update Max and min in an array.cpp

--- arrays /Max and min in an array.cpp
+++ arrays /Max and min in an array.cpp
@@ -1,1 +1,21 @@
+class Solution {
+  public:
+    vector<int> getMinMax(vector<int> &arr) {
+        // code here
+             int min_val = arr[0];
+        int max_val = arr[0];
+        
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] < min_val) {
+                min_val = arr[i];
+            }
+            if (arr[i] > max_val) {
+                max_val = arr[i];
+            }
+        }
+        
+        return {min_val, max_val};
 
+        
+    }
+};
