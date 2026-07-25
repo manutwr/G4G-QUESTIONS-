6eb559d68e5a90b5fdadc6358117830919ab4172
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-25 08:28:57 +0530

    Update Reverse an array.cpp

--- arrays /Reverse an array.cpp
+++ arrays /Reverse an array.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        int left = 0;
+        int right = arr.size() - 1;
+        
+        while (left < right) {
+            swap(arr[left], arr[right]);
+            
+            
+            left++;
+            right--;
+        }
+    }
+};
