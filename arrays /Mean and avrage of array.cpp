7aaf100b77e545bef8936f5ca9de2ef4cd4f667c
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-12 20:36:23 +0530

    Update Mean and avrage of array.cpp

--- arrays /Mean and avrage of array.cpp
+++ arrays /Mean and avrage of array.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+  public:
+    int findMean(vector<int>& arr) {
+        long long sum = 0;
+        int n = arr.size();
+        
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        
+        return sum / n;  // when you find sum you have to find mean by dividig sum by n 
+    }
+};
