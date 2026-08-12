0cf934db40f48d34d79b263101ceb282cb920f46
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-12 17:38:36 +0530

    Update Median of an array.cpp

--- arrays /Median of an array.cpp
+++ arrays /Median of an array.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     double findMedian(vector<int> &arr) {
         
-        sort(arr.begin(), arr.end());
+        sort(arr.begin(), arr.end()); // sort an array
         
-        int n = arr.size();
+        int n = arr.size(); // size declear 
         
         
-        if (n % 2 != 0) {
+        if (n % 2 != 0) { // agar array odd hai to arr n/2 return kro
             return arr[n / 2];
         }
     
-        return (arr[(n / 2) - 1] + arr[n / 2]) / 2.0;
+        return (arr[(n / 2) - 1] + arr[n / 2]) / 2.0; // agar array even hai to -1 kro
     }
 };
