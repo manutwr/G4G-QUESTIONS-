9792b0a4890162ae87670ec1254b684419b7d5da
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-16 09:33:23 +0530

    Update Cheak palindrome.cpp

--- String /Cheak palindrome.cpp
+++ String /Cheak palindrome.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(string& s) {
-        int left = 0;
-        int right = s.size() - 1;
+        int left = 0;   // left ko zero maan ke aage badho
+        int right = s.size() - 1; // right ki limit -1 hai
 
-        while (left < right) {
-            if (s[left] != s[right]) {
+        while (left < right) { // loop jb tk chalao jb tk 
+            if (s[left] != s[right]) { // agar left right ke eql nii hai toh false return maar do
                 return false;
             }
             left++;
