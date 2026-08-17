aa457b0643bab4e008f8c3366475ab74e8cefac5
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-17 09:12:22 +0530

    Create Divisble by 13.cpp

---
+++ MATHS/Medim/Divisble by 13.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool divby13(string &s) {
+        int remainder = 0;
+
+        for (char c : s) {
+          
+            int digit = c - '0';
+
+            remainder = (remainder * 10 + digit) % 13;
+        }
+
+       
+        return remainder == 0;
+    }
+};
