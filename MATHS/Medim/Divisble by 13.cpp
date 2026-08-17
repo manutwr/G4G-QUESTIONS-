3c631921811ecc8563921529490db9884f5f9121
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-17 09:19:54 +0530

    Update Divisble by 13.cpp

--- MATHS/Medim/Divisble by 13.cpp
+++ MATHS/Medim/Divisble by 13.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool divby13(string &s) {
-        int remainder = 0;
+        int remainder = 0; // didgit by digit checking
 
-        for (char c : s) {
+        for (char c : s) { // loop
           
-            int digit = c - '0';
+            int digit = c - '0'; // convert into digit
 
-            remainder = (remainder * 10 + digit) % 13;
+            remainder = (remainder * 10 + digit) % 13 ; // checking digit by digit on loop 
         }
 
        
-        return remainder == 0;
+        return remainder == 0; // agar finalremaind ki value 0 ho gyi to loop return maar dena 
     }
 };
