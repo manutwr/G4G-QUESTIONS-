895940784b8b46ee7d13e595fe03b8bc41980ffb
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-17 09:00:31 +0530

    Update Find n ways.cpp

--- MATHS/Medim/Find n ways.cpp
+++ MATHS/Medim/Find n ways.cpp
@@ -2,14 +2,14 @@ class Solution {
   public:
     int countWays(int n) {
         // code here
-        if (n <= 2 ){
+        if (n <= 2 ){ // agar n 2 se chhota hai seedhi  se km hai to n hi combinations bnenge
             return n ;
         }
-        int  a= 1, b = 2;
+        int  a= 1, b = 2; // a aur b ko value assign kro 
         for (int i = 3 ; i <= n; i++){
-            int c = a + b ;
-            a =b;
-            b=c;
+            int c = a + b ; // c ka mtlb n-1 tk + n-2 tk kr steps ko add krna 
+            a =b; // a me b ko ddaal do
+            b=c; // b me c ko 
             
         }
         return b;
