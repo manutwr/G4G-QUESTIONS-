c520934948f09f823bd23ed3682fe962edd938dc
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-20 08:33:40 +0530

    Update Divisble by 3.cpp

--- arrays /Divisble by 3.cpp
+++ arrays /Divisble by 3.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+  public:
+    bool isPossible(vector<int>& arr) {
+        // code here
+        long long sum = 0;
+        for (int i=0 ; i< arr.size(); i++){
+            sum = sum + arr[i];  // adding 
+        }
+         if (sum%3 == 0){
+             return true;
+         }
+         else {
+             return false;
+         }
+    }
+};
