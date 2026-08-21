47e09471d65093bd8c119710b5f003ea19ede084
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-21 10:33:53 +0530

    Update Binary search.cpp

--- arrays /Binary search.cpp
+++ arrays /Binary search.cpp
@@ -1,11 +1,11 @@
 class Solution {
   public:
     bool binarySearch(vector<int>& arr, int k) {
-        int start = 0;
-        int end = arr.size() - 1;
+        int start = 0;  // stat from 0
+        int end = arr.size() - 1; // end from arr-1
 
-        while (start <= end ) {
-            int mid = start + (end - start) / 2;
+        while (start <= end ) { // jb tk end bada na ho jaye strat ke
+            int mid = start + (end - start) / 2;// mid calculation
 
             if (arr[mid] == k) {
                 return true;
