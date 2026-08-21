9ad5154de3a799ef2bd89b086aec1237a500373f
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-21 10:35:05 +0530

    Update Binary search.cpp

--- arrays /Binary search.cpp
+++ arrays /Binary search.cpp
@@ -7,14 +7,14 @@ class Solution {
         while (start <= end ) { // jb tk end bada na ho jaye strat ke
             int mid = start + (end - start) / 2;// mid calculation
 
-            if (arr[mid] == k) {
+            if (arr[mid] == k) { // agar elemen mid hi hai to 
                 return true;
             }
-            else if (arr[mid] < k) {
-                start = mid + 1;
+            else if (arr[mid] < k) { // agar target mid se bada hai
+                start = mid + 1;// to mid+1 s3 start kr do
             }
-            else {
-                end = mid - 1;
+            else { 
+                end = mid - 1; // agar mid se chooto ha to mid-1 se start krna hai 
             }
         }
 
