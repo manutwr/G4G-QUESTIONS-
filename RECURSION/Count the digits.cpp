88fdb836bb1d81ad94665c76efaed38a30825f18
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-25 08:51:00 +0530

    Update Count the digits.cpp

--- RECURSION/Count the digits.cpp
+++ RECURSION/Count the digits.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int countDigits(int n) {
       
-        if (n == 0) return 1;
+        if (n == 0) return 1; // base case
 
-        int count = 0;
-        while (n > 0) {
-            count++;
-            n = n / 10; }
+        int count = 0; // count e 0 daalo
+        while (n > 0) { // agar 0 chhota hai n se
+            count++; // count ko aage badhao
+            n = n / 10; } // n ko 10 se divide kr do har digit ko bhr nikalne ke lie
 
         return count;
     }
